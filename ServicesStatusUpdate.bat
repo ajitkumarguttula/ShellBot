b795f970d6ad6e1a67f1da1803d50efa30aa3bcf
Ajit <32298821+ajitkumarguttula@users.noreply.github.com>
2017-11-30 13:07:03 +0100

    To validate the services status

---
+++ ServicesStatusUpdate.bat
@@ -0,0 +1,3 @@
+(sc \\Server01  query  CommerceDataExchangeAsyncClientService
+sc \\Server02 query  CommerceDataExchangeAsyncClientService
+sc \\Server03 query  CommerceDataExchangeAsyncClientService) > ServicesStatusUpdate.txt
